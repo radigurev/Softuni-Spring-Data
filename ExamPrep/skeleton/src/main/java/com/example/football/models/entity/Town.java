5a095f5a8489b734e7cb5572bc8ed41ce9587ff3
radigurev <radi.gurev@gmail.com>
2022-01-20 12:58:47 +0200

    Prep Exam 2

--- ExamPrep/skeleton/src/main/java/com/example/football/models/entity/Town.java
+++ ExamPrep/skeleton/src/main/java/com/example/football/models/entity/Town.java
@@ -1,4 +1,64 @@
 package com.example.football.models.entity;
 
-public class Town extends BaseEntity{
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "towns")
+public class Town extends BaseEntity {
+    private String name;
+    private int population;
+    private String travelGuide;
+    private Set<Player> players;
+    private Set<Team> teams;
+    public Town() {
+    }
+
+    @Column(unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Column(name = "travel_guide")
+    public String getTravelGuide() {
+        return travelGuide;
+    }
+
+    public void setTravelGuide(String travelGuide) {
+        this.travelGuide = travelGuide;
+    }
+    @OneToMany(mappedBy = "town")
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+    @OneToMany(mappedBy = "town")
+    public Set<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
 }
